bb842abd786e483e8d2c39139379942e3d13a9ae
agustinpittari <agustinpittari4@hotmail.com>
2020-02-01 19:00:02 -0300

    CRUD usuario

---
+++ controllers/userController.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcrypt')
+const { check, validationResult, body } = require('express-validator')
+
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+module.exports = {
+    index: (req, res) => {
+        res.render('User/userList', {usuarios: users})
+    },
+    createForm: (req, res) => {
+        res.render('User/userCreate')
+    },
+    create: (req, res) => {
+
+        let usuario = {
+            id : users.length + 1,
+            first_name : req.body.first_name,
+            last_name : req.body.last_name,
+            email : req.body.email,
+            gender: req.body.gender,
+            password: bcrypt.hashSync(req.body.password, 10)
+        }
+        users.push(usuario)
+
+        fs.writeFileSync(usersFilePath, JSON.stringify(users))
+
+        res.redirect('/users')
+    },
+    editForm: (req, res) => {
+        let usuario = users.find(function(u){
+            return u.id == req.params.id
+        })
+
+        res.render('User/userEdit', {usuario: usuario})
+    },
+    edit: (req, res) => {
+        let arrayIndex
+
+		let user = users.find(function (p, index) {
+			if (p.id == req.params.id) {
+				arrayIndex = index
+				return true
+			}
+
+			return false
+		})
+		
+		let editado = {
+			...user,
+            first_name : req.body.first_name,
+            last_name : req.body.last_name,
+            email : req.body.email,
+            gender: req.body.gender,
+            password: bcrypt.hashSync(req.body.password, 10)
+		}
+
+		users[arrayIndex] = editado
+
+		fs.writeFileSync(usersFilePath, JSON.stringify(users))
+
+		res.redirect('/users/' + req.params.id)
+    },
+    detail: (req, res) => {
+        let usuario = users.find(function(u){
+            return u.id == req.params.id
+        })
+        res.render('User/userDetail', {usuario: usuario})
+    },
+    loginForm: (req, res) => {
+        res.render('User/userLogin')
+
+    },
+    login: (req, res) => {
+        for (let i = 0; i < users.length; i++) {
+            if (req.body.email === users[i].email && bcrypt.compareSync(req.body.password, users[i].password)){  
+                res.redirect('/users/' + users[i].id)
+            } 
+        }
+        res.render('User/userlogin', {info: req.body})
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/userController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.index);
 
+router.get('/create', userController.createForm)//Ruta que muestra formulario de creacion de usuario
+router.post('/create', userController.create)//Ruta para almacenar el usuario 
+
+router.get('/edit/:id', userController.editForm)//Formulario de edicion
+router.put('/edit/:id', userController.edit) //Guardado de usuario editado
+
+router.get('/login', userController.loginForm)//Login de usuario
+router.post('/login', userController.login)//validacion de login
+
+router.get('/:id', userController.detail)//Detalle de usuario
 module.exports = router;

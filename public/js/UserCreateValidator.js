7954cdc367986e408fee742299a8e372c950967d
Sebarg91 <sebastian.gaboto@hotmail.com>
2020-03-10 16:37:45 -0300

    uservalidator

---
+++ public/js/UserCreateValidator.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form#userCreate')
+
+const firstname = document.querySelector('input[name=first_name]')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault
+})

78e4117d6c80c36bc74ed8382f978b227d15bc96
nicolas <lopez>
2020-03-10 16:57:07 -0300

    Product validator terminado

--- public/js/productsValidator.js
+++ public/js/productsValidator.js
@@ -1,11 +1,22 @@
 const form = document.querySelector('#productCreate')
 
 const first_name = document.querySelector('input[name=nombre]')
+const description = document.querySelector('input[name=descripcion]')
 
 first_name.addEventListener('blur', function(){
-    if(validator.isEmpty(this.value, {ignore_whitespace:true })){
+    if(validator.isLength(this.value, {max:4 })){
         this.classList.add('is-invalid')
-        
+
+    } else {
+        this.classList.remove('is-invalid')
+    }
+})
+description.addEventListener('blur', function(){
+    if(validator.isLength(this.value, {max:19 })){
+        this.classList.add('is-invalid')
+
+    } else {
+        this.classList.remove('is-invalid')
     }
 })
 

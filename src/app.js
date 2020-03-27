d3fb5d86f1a1f0272ebba9244b117ac1e29fdbe8
agustinpittari <agustinpittari4@hotmail.com>
2020-03-26 23:26:19 -0300

    Cambio de orden

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const fs = require('fs')
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const methodOverride = require('method-override')
@@ -22,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(methodOverride('_method')) //Procesamientos PUT y DELETE
 app.use(session({secret: 'Secreto'}))
 app.use(rememberMiddleware)

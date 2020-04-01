5ffd17e4d8aaee951bf7ad1148fdbc6affd9536d
agustinpittari <agustinpittari4@hotmail.com>
2020-04-01 18:12:38 -0300

    Arreglo de api

--- react-dashboard/src/components/container-Fluid/SmallCard.js
+++ react-dashboard/src/components/container-Fluid/SmallCard.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react'
 
 class SmallCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            cantidad : ""
+        }
+    }
 
     apiCall(url, cons){
         fetch(url)
@@ -18,13 +24,22 @@ class SmallCard extends Component{
     }
 
     render(){
+
+        let contenido
+
+        if (this.state.cantidad == ""){
+            contenido = <p>Cargando...</p>
+        } else {
+            contenido = this.state.cantidad
+        }
+
         return(
             <div className="card border-left-primary shadow h-100 py-2">
                 <div className="card-body">
                     <div className="row no-gutters align-items-center">
                         <div className="col mr-2">
                             <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">{this.props.title}</div>
-                            <div className="h5 mb-0 font-weight-bold text-gray-800">{this.props.value}</div>
+                            <div className="h5 mb-0 font-weight-bold text-gray-800">{contenido}</div>
                         </div>
                         <div className="col-auto">
                             <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
